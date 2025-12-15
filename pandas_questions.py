ced16a202bb064774958913b8c7fbeba7e62cb69
root <root@LAPTOP-J8FAVI41.localdomain>
2025-12-15 20:14:11 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -12,6 +12,7 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
+
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
     referendum = pd.read_csv('data/referendum.csv', sep=';')
